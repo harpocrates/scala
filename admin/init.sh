548b54bb84c6168275817bf8ab4850c00621af8c
Lukas Rytz <lukas.rytz@gmail.com>
2020-12-11 14:29:41 +0100

    wip

--- admin/init.sh
+++ admin/init.sh
@@ -10,11 +10,16 @@ sensitive() {
   perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/credentials-sonatype     > ~/.credentials-sonatype
   perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/sonatype-curl            > ~/.sonatype-curl
 
-  openssl aes-256-cbc -md md5 -d -pass "pass:$GPG_SUBKEY_SECRET" -in files/gpg_subkey.enc | gpg --import
+  openssl aes-256-cbc -md md5 -pass "pass:$GPG_SUBKEY_SECRET_DUMMY" -in files/gpg_subkey.enc | gpg --import
 }
 
+GPG_SUBKEY_SECRET_DUMMY="nix"
+SONA_PASS_DUMMY="nix"
+SONA_USER_DUMMY="nix"
+PRIVATE_REPO_PASS_DUMMY="nix"
+
 # don't let anything escape from the sensitive part (e.g. leak environment var by echoing to log on failure)
-sensitive >/dev/null 2>&1
+sensitive
 
 # just to verify
 gpg --list-keys
